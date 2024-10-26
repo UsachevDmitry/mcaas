cbd200be559f59bae5da88790b5570f66fae1db3
dmitry.usachev <dmitry.usachev@indeed-id.com>
2024-10-26 16:12:21 +0300

    fix status code for invalid value after autotest

--- cmd/server/handlePostMetrics.go
+++ cmd/server/handlePostMetrics.go
@@ -16,25 +16,23 @@ func handlePostMetrics(w http.ResponseWriter, r *http.Request) {
 	dataType = mux.Vars(r)["type"]
 	name = mux.Vars(r)["name"]
 	value = mux.Vars(r)["value"]
+    // Проверяем данные
     if name == "" {
         w.WriteHeader(http.StatusNotFound)  
         return
     }
     if _, err := strconv.ParseInt(value, 10, 64); err != nil && dataType == "counter" {
-        w.WriteHeader(http.StatusNotFound)  
+        w.WriteHeader(http.StatusBadRequest)  
         return
     }
     if _, err := strconv.ParseFloat(value, 64); err != nil && dataType == "gauge" {
-        w.WriteHeader(http.StatusNotFound)  
+        w.WriteHeader(http.StatusBadRequest)  
         return
     }
     // Проверяем type данных
     if dataType == "gauge" {
-        // fmt.Println("key= ",key)
-        //fmt.Println("name empty=",name)
         if name == "" {
-            w.WriteHeader(http.StatusNotFound)
-            //fmt.Println("bad")        
+            w.WriteHeader(http.StatusNotFound)    
             return
         } else {                
             f, _ := strconv.ParseFloat(value, 64)
@@ -43,14 +41,11 @@ func handlePostMetrics(w http.ResponseWriter, r *http.Request) {
             w.WriteHeader(http.StatusOK)
             return
         }
-        //w.WriteHeader(http.StatusBadRequest)
     } else if dataType == "counter" {
         _, exists := Data.MetricsCounter[name]
         if !exists { 
-            //fmt.Println("Not exist ", name, Data.Metrics[name])
             f, _ := strconv.ParseInt(value, 10, 64)
             Data.MetricsCounter[name] = counter(f)
-            //w.WriteHeader(http.StatusNotFound)
             w.WriteHeader(http.StatusOK)
             return
         } else { 
