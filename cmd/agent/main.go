3042a01677b7daeee1a406d72aaad113fb9fa5e2
dmitry.usachev <dmitry.usachev@indeed-id.com>
2024-10-27 22:27:22 +0300

    add random and time

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,38 +3,57 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"time"
+    "math/rand"
 )
 
-func main() {    
-    var m runtime.MemStats
-    runtime.ReadMemStats(&m)
+func main() {
+    var pollInterval time.Duration = 2
+	go updateData(pollInterval)
+    sendData()
+}
 
-    Data.UpdateGauge("Alloc", gauge(m.Alloc))
-    Data.UpdateGauge("BuckHashSys", gauge(m.BuckHashSys))
-    Data.UpdateGauge("Frees",  gauge(m.Frees))
-    Data.UpdateGauge("GCCPUFraction",  gauge(m.GCCPUFraction))
-    Data.UpdateGauge("GCSys",  gauge(m.GCSys))
-    Data.UpdateGauge("HeapAlloc",  gauge(m.HeapAlloc))
-    Data.UpdateGauge("HeapIdle",  gauge(m.HeapIdle))
-    Data.UpdateGauge("HeapInuse",  gauge(m.HeapInuse))
-    Data.UpdateGauge("HeapObjects",  gauge(m.HeapObjects))
-    Data.UpdateGauge("HeapReleased",  gauge(m.HeapReleased))
-    Data.UpdateGauge("HeapSys",  gauge(m.HeapSys))
-    Data.UpdateGauge("LastGC",  gauge(m.LastGC))
-    Data.UpdateGauge("Lookups",  gauge(m.Lookups))
-    Data.UpdateGauge("MCacheInuse",  gauge(m.MCacheInuse))
-    Data.UpdateGauge("MCacheSys",  gauge(m.MCacheSys))
-    Data.UpdateGauge("MSpanInuse",  gauge(m.MSpanInuse))
-    Data.UpdateGauge("MSpanSys",  gauge(m.MSpanSys))
-    Data.UpdateGauge("Mallocs",  gauge(m.Mallocs))
-    Data.UpdateGauge("NextGC",  gauge(m.NextGC))
-    Data.UpdateGauge("NumForcedGC",  gauge(m.NumForcedGC))
-    Data.UpdateGauge("OtherSys",  gauge(m.OtherSys))
-    Data.UpdateGauge("PauseTotalNs",  gauge(m.PauseTotalNs))
-    Data.UpdateGauge("StackInuse",  gauge(m.StackInuse))
-    Data.UpdateGauge("StackSys",  gauge(m.StackSys))
-    Data.UpdateGauge("Sys",  gauge(m.Sys))
-    Data.UpdateGauge("TotalAlloc",  gauge(m.TotalAlloc))
+func sendData() {
+    for {
+        fmt.Println(Data)
+        time.Sleep(10 * time.Second)
+    }
+}
 
-    fmt.Println(Data)
+func updateData(pollInterval time.Duration) {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+    var i = 0    
+	for {
+		Data.UpdateGauge("Alloc", gauge(m.Alloc))
+		Data.UpdateGauge("BuckHashSys", gauge(m.BuckHashSys))
+		Data.UpdateGauge("Frees", gauge(m.Frees))
+		Data.UpdateGauge("GCCPUFraction", gauge(m.GCCPUFraction))
+		Data.UpdateGauge("GCSys", gauge(m.GCSys))
+		Data.UpdateGauge("HeapAlloc", gauge(m.HeapAlloc))
+		Data.UpdateGauge("HeapIdle", gauge(m.HeapIdle))
+		Data.UpdateGauge("HeapInuse", gauge(m.HeapInuse))
+		Data.UpdateGauge("HeapObjects", gauge(m.HeapObjects))
+		Data.UpdateGauge("HeapReleased", gauge(m.HeapReleased))
+		Data.UpdateGauge("HeapSys", gauge(m.HeapSys))
+		Data.UpdateGauge("LastGC", gauge(m.LastGC))
+		Data.UpdateGauge("Lookups", gauge(m.Lookups))
+		Data.UpdateGauge("MCacheInuse", gauge(m.MCacheInuse))
+		Data.UpdateGauge("MCacheSys", gauge(m.MCacheSys))
+		Data.UpdateGauge("MSpanInuse", gauge(m.MSpanInuse))
+		Data.UpdateGauge("MSpanSys", gauge(m.MSpanSys))
+		Data.UpdateGauge("Mallocs", gauge(m.Mallocs))
+		Data.UpdateGauge("NextGC", gauge(m.NextGC))
+		Data.UpdateGauge("NumForcedGC", gauge(m.NumForcedGC))
+		Data.UpdateGauge("OtherSys", gauge(m.OtherSys))
+		Data.UpdateGauge("PauseTotalNs", gauge(m.PauseTotalNs))
+		Data.UpdateGauge("StackInuse", gauge(m.StackInuse))
+		Data.UpdateGauge("StackSys", gauge(m.StackSys))
+		Data.UpdateGauge("Sys", gauge(m.Sys))
+		Data.UpdateGauge("TotalAlloc", gauge(m.TotalAlloc))
+        i++
+        Data.AddCounter("PollCount", counter(i))
+        Data.UpdateGauge("RandomValue", gauge(rand.Float64()))
+		time.Sleep(pollInterval * time.Second)
+	}
 }
