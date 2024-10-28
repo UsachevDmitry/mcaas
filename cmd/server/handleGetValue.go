17cd321bb2fd4bde05b3066d70d8f60b1a84cfdf
dmitry.usachev <dmitry.usachev@indeed-id.com>
2024-10-28 17:19:35 +0300

    add handle for Get Value

---
+++ cmd/server/handleGetValue.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func handleGetValue(w http.ResponseWriter, r *http.Request) {
+	var dataType string
+	var name string
+
+	dataType = mux.Vars(r)["type"]
+	name = mux.Vars(r)["name"]
+	// Проверяем type данных
+	if dataType == "gauge" {
+		value, exists := Data.GetGauge(name)
+		if !exists {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		fmt.Fprintf(w, "<html><body><h1>Gauge</h1><p>name: %s<br>value: %v</p></body></html>", name, value)
+		w.WriteHeader(http.StatusOK)
+		return
+	} else if dataType == "counter" {
+		value, exists := Data.GetCounter(name)
+		if !exists {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		fmt.Fprintf(w, "<html><body><h1>Counter</h1><p>name: %s<br>value: %v</p></body></html>", name, value)
+		w.WriteHeader(http.StatusOK)
+		return
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
