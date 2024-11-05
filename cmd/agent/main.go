5110b5d7be2b1bb489a25cb95878f1864e810203
dmitry.usachev <dmitry.usachev@indeed-id.com>
2024-11-05 23:16:23 +0300

    add wait group

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,11 +3,36 @@ package main
 import (
 	"github.com/UsachevDmitry/mcaas/cmd/agent/internal"
 	"time"
+	"sync"
 )
 
 func main() {
+	// Создание группы ожидания
+	var wg sync.WaitGroup
+
+	// Получение конфигурации
 	internal.GetConfig()
-	go internal.UpdateData(time.Duration(*internal.PollInterval))
-	go internal.SendDataCounter(time.Duration(*internal.ReportInterval))
-	internal.SendDataGauge(time.Duration(*internal.ReportInterval))
+
+	// Добавление количества подпрограмм в группу ожидания
+	wg.Add(3)
+
+	// Запуск подпрограмм
+	go func() {
+		defer wg.Done()
+		internal.UpdateData(time.Duration(*internal.PollInterval))
+	}()
+
+	go func() {
+		defer wg.Done()
+		internal.SendDataCounter(time.Duration(*internal.ReportInterval))
+	}()
+
+	go func() {
+		defer wg.Done()
+		internal.SendDataGauge(time.Duration(*internal.ReportInterval))
+	}()
+
+	// Ожидание завершения всех подпрограмм
+	wg.Wait()
+
 }
