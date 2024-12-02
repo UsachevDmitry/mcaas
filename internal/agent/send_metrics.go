31105125b68116f8b8115c34f44eb8a846a6d0a2
dmitry.usachev <dmitry.usachev@indeed-id.com>
2024-12-02 23:25:38 +0300

    fix agent retry

--- internal/agent/send_metrics.go
+++ internal/agent/send_metrics.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -29,40 +30,33 @@ func SendMetrics(reportInterval time.Duration) {
 			fmt.Println("Error compress jsonBody", err)
 			continue
 		}
-
-		req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(compressedJSONBody))
-		if err != nil {
-			fmt.Println("Error creating request:", err)
-			continue
-		}
-		req.Header.Set("Content-Encoding", "gzip")
-		req.Header.Set("Content-Type", "application/json")
-		client := &http.Client{}
+		var countRetry = 1
 		for i := 1; i < 6; i += 2 {
+			ctxWithTimeout, cancel := context.WithTimeout(context.Background(), time.Duration(i)*time.Second)
+			req, err := http.NewRequestWithContext(ctxWithTimeout, http.MethodPost, url, bytes.NewBuffer(compressedJSONBody))
+			if err != nil {
+				cancel()
+				fmt.Println("Error creating request:", err)
+				if i == 5 {
+					fmt.Println("All retries exhausted, exiting...")
+					break
+				}
+				fmt.Printf("Retry %v...", countRetry)
+				countRetry++
+				continue
+			}
+			req.Header.Set("Content-Encoding", "gzip")
+			req.Header.Set("Content-Type", "application/json")
+			client := &http.Client{}
 			resp, err := client.Do(req)
 			if err != nil {
 				fmt.Println("Error sending request:", err)
-				fmt.Printf("Retry after %v second\n", i)
-				time.Sleep(time.Second * time.Duration(i))
-				if i == 5 {
-					fmt.Println("All retries exhausted, exiting...")
-					break
-				}
-				continue
-			}
-
 			if resp.StatusCode != http.StatusOK {
 				fmt.Println("Error status:", resp.StatusCode)
-				time.Sleep(time.Second * time.Duration(i))
-				if i == 5 {
-					fmt.Println("All retries exhausted, exiting...")
-					break
-				}
-				continue
-			} else {
-				i = 6
 			}
 			resp.Body.Close()
+			cancel()
+			}
 		}
 	}
 }
